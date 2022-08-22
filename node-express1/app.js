9fd0c7a99b012e5c181431de624ec56bc5477850
overroading <overroading@gmail.com>
2022-08-22 14:56:11 +0900

    node-express

--- node-express1/app.js
+++ node-express1/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const path = require('path');
+
+dotenv.config();
 
 const app = express();
 //express 모듈 할당
@@ -7,12 +13,39 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 //서버가 실행될 포트를 설정
 
-app.get('/', (req, res) => {
-    //res.send('Hello, Express');
-    res.sendFile(path.join(__dirname, '/index.html'));
-    //path 모듈을 이용하여 sendFile메서드에서 파일을 불러옴
+app.use(morgan('combined'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
+
+app.use((req, res, next) => {
+//next 매개변수 : 다음 미들웨어로 넘어가는 함수
+//next를 실행하지 않으면 다음 미들웨어가 실행되지 않음
+    console.log('모든 요청을 다 실행됩니다.');
+    next();
 });
-//GET 요청이 올 때 동작
+app.get('/', (req, res, next) => {
+    console.log('GET / 요청에서만 실행됩니다.');
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 이동합니다.');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
