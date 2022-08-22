42b7ed6a84cb2d51b9cc18092e1abe2c1afbb5fb
overroading <overroading@gmail.com>
2022-08-22 23:36:51 +0900

    node-express

--- node-express1/app.js
+++ node-express1/app.js
@@ -8,6 +8,9 @@ const path = require('path');
 
 dotenv.config();
 
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
+
 const app = express();
 //express 모듈 할당
 
@@ -32,8 +35,45 @@ app.use(session({
     name: 'session-cookie',
 }));
 
-const multer = require('multer');
+app.use('/', indexRouter);
+app.use('/user', userRouter);
 
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+const multer = require('multer');
+const fs = require('fs');
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done){
+            done(null, 'uploads/');
+        },
+        filename(req, file, done){
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    },
+});
+
+try{
+    fs.readdirSync('uploads');
+}catch(error){
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'multipart.html')); 
+});
+app.post('/upload', upload.fields([{ name: 'image1' }, { name: 'image2' }]), (req, res) => {
+    console.log(req.files, req.body);
+    res.send('ok');
+});
 
 app.use((req, res, next) => {
 //next 매개변수 : 다음 미들웨어로 넘어가는 함수
